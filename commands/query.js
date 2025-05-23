be12ccb0a2cf54fce88695c6e632a5da269b4096
michaelsmetsker2 <michaelsmetsker2@gmail.com>
2025-05-23 15:17:36 -0700

    finished query command

--- commands/query.js
+++ commands/query.js
@@ -1,17 +1,32 @@
 import fs from 'node:fs/promises';
+import { AttachmentBuilder } from 'discord.js';
+import { sleep } from '../utils.js';
 
 export const query = async (interaction) => {
     try {
+        const sender = interaction.user.tag; // or .username
         const filePath = './responces.txt'
         const outpath = './temp/query.txt'
+        const wavPath = './queryOutput.wav'
 
         const data = await fs.readFile(filePath, 'utf-8');
-        const lines = data.split(/\r?n/).filter(line => line.trim() !== '');
+        const lines = data.split(/\r?\n/).filter(line => line.trim() !== '');
 
         const randomLine = lines[Math.floor(Math.random() * lines.length)];
 
         await fs.writeFile(outpath, randomLine, 'utf-8');
-        await interaction.reply('test') 
+
+
+        await sleep(50);
+
+        const file = new AttachmentBuilder(wavPath);
+
+        await sleep(200);
+        await interaction.reply({
+            content: "",
+            files: [file]
+        }) 
+        console.log("query success, from: ", sender)
     } catch (error) {
         console.error(error);
         await interaction.reply('sumthin bad happened');
