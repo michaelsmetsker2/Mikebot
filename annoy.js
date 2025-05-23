26a6183550e204711531019820ae25cf9532a1d2
michaelsmetsker2 <michaelsmetsker2@gmail.com>
2025-05-23 12:32:34 -0700

    lowered masx character count on annoy worked on new query command

--- annoy.js
+++ annoy.js
@@ -5,15 +5,14 @@ export const annoy = async (interaction) => {
     const message = interaction.options.getString('message');
     const sender = interaction.user.tag; // or .username
 
-    if (message.length > 300) {
+    if (message.length > 250) {
       await interaction.reply('fuck you i\'m not listening to all of that');
       return;
     }
 
-
     console.log(sender, 'says:', message);
 
-    await fs.writeFile('./tts.txt', message);
+    await fs.writeFile('./temp/tts.txt', message);
     console.log('file write successful');
 
     await interaction.reply('Message delivered, it has been done!');
