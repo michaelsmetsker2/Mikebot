c76346e60380215796278be044e37b83df718c3d
michaelsmetsker2 <michaelsmetsker2@gmail.com>
2025-05-23 23:21:58 -0700

    added queueing for tts commands

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-// index.js
+/**
+ * @fileoverview
+ * Main file of mikebot, handles command activations and initializing the bot
+ */
+
 import { Client, GatewayIntentBits, Events } from 'discord.js';
 import { annoy } from './commands/annoy/annoy.js';
 import { query } from './commands/query/query.js';
@@ -6,9 +10,38 @@ import 'dotenv/config';
 
 const client = new Client({ 
     intents: [
-        GatewayIntentBits.Guilds
+        GatewayIntentBits.Guilds //lets the bot join servers
     ]
 });
+
+const ttsQueue = []; //queue of tts command calls so that they dont run at the same time
+let activeQueue = false;
+
+//process tts commands one at a time
+const processQueue = async () => {
+  if (activeQueue) return;
+  activeQueue = true;
+
+  while (ttsQueue.length > 0) {
+    const interaction = ttsQueue.shift();
+    
+    try {
+      if (interaction.commandName === 'annoy') {
+        await annoy(interaction);
+      }
+      else if (interaction.commandName === 'query') {
+        await query(interaction);
+      }
+    }
+    catch (error) {
+      console.error('error processing a tts interation: ', error);
+      if (!interaction.replied && !interaction.deferred) {
+        await interaction.reply('error while executing the commmand');
+      }
+    }
+  }
+  activeQueue = false;
+}
 
 client.once(Events.ClientReady, c => {
   console.log(`Logged in as ${c.user.tag}`);
@@ -16,12 +49,12 @@ client.once(Events.ClientReady, c => {
 
 client.on(Events.InteractionCreate, async interaction => {
   if (!interaction.isChatInputCommand()) return;
-  
-  if (interaction.commandName === 'annoy') {
-    await annoy(interaction);
-  }
-  else if (interaction.commandName === 'query') {
-    await query(interaction);
+
+  if (interaction.commandName === 'honk') {
+
+  } else { //must be a tts command then+-
+    ttsQueue.push(interaction);
+    processQueue();
   }
 
 });
