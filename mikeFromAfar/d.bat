fefebd39e2f8c43ad018ae8868bdf11caf68bf33
root <root@mikebot.local>
2025-06-06 04:47:42 +0000

    added it back lol

---
+++ mikeFromAfar/d.bat
@@ -0,0 +1,10 @@
+@echo off
+echo Registering AcaTts for Sapi 5 layer.
+if exist %systemroot%\syswow64\regsvr32.exe (
+%systemroot%\syswow64\regsvr32.exe "%~dp0\AcaTtsSapi5.dll"
+%systemroot%\system32\regsvr32.exe "%~dp0\AcaTtsSapi5.64.dll"
+) else (
+%systemroot%\system32\regsvr32.exe "%~dp0\AcaTtsSapi5.dll"
+)
+pause
+exit
