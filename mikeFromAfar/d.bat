51a77a638a9c79df98ac3424903d397afefd0706
michaelsmetsker2 <michaelsmetsker2@gmail.com>
2025-06-05 19:11:56 -0700

    fully removed mikefromafar as wine does not like it

--- mikeFromAfar/d.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Registering AcaTts for Sapi 5 layer.
-if exist %systemroot%\syswow64\regsvr32.exe (
-%systemroot%\syswow64\regsvr32.exe /s "%~dp0\AcaTtsSapi5.dll"
-%systemroot%\system32\regsvr32.exe /s "%~dp0\AcaTtsSapi5.64.dll"
-) else (
-%systemroot%\system32\regsvr32.exe /s "%~dp0\AcaTtsSapi5.dll"
-)
-exit
