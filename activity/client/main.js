25cbba854fc7987689e2348f54a5ad47284f6025
michaelsmetsker2 <michaelsmetsker2@gmail.com>
2025-06-11 15:54:51 -0700

    displays channel name

--- activity/client/main.js
+++ activity/client/main.js
@@ -11,6 +11,7 @@ const discordSdk = new DiscordSDK(import.meta.env.VITE_DISCORD_CLIENT_ID);
 setupDiscordSdk().then(() => {
   console.log("Discord SDK is authenticated");
 
+  appendVoiceChannelName();
   // We can now make API calls within the scopes we requested in setupDiscordSDK()
   // Note: the access_token returned is a sensitive secret and should be treated as such
 });
@@ -63,3 +64,25 @@ document.querySelector('#app').innerHTML = `
     <h1>Hello, World!</h1>
   </div>
 `;
+
+async function appendVoiceChannelName() {
+  const app = document.querySelector('#app');
+
+  let activityChannelName = 'Unknown';
+
+  // Requesting the channel in GDMs (when the guild ID is null) requires
+  // the dm_channels.read scope which requires Discord approval.
+  if (discordSdk.channelId != null && discordSdk.guildId != null) {
+    // Over RPC collect info about the channel
+    const channel = await discordSdk.commands.getChannel({channel_id: discordSdk.channelId});
+    if (channel.name != null) {
+      activityChannelName = channel.name;
+    }
+  }
+
+  // Update the UI with the name of the current voice channel
+  const textTagString = `Activity Channel: "${activityChannelName}"`;
+  const textTag = document.createElement('p');
+  textTag.textContent = textTagString;
+  app.appendChild(textTag);
+}
