fbc1ff886e8da5a057d4e3df22645a20f7121f87
michaelsmetsker2 <michaelsmetsker2@gmail.com>
2025-06-12 18:50:31 -0700

    append guild avatar

--- activity/client/main.js
+++ activity/client/main.js
@@ -13,6 +13,7 @@ setupDiscordSdk().then(() => {
 
   // We can now make API calls within the scopes we requested in setupDiscordSDK()
   appendVoiceChannelName();
+  appendGuildAvatar();
   // Note: the access_token returned is a sensitive secret and should be treated as such
 });
 
@@ -85,4 +86,34 @@ async function appendVoiceChannelName() {
   const textTag = document.createElement('p');
   textTag.textContent = textTagString;
   app.appendChild(textTag);
+}
+
+async function appendGuildAvatar() {
+  const app = document.querySelector('#app');
+
+  // 1. From the HTTP API fetch a list of all of the user's guilds
+  const guilds = await fetch(`https://discord.com/api/v10/users/@me/guilds`, {
+    headers: {
+      // NOTE: we're using the access_token provided by the "authenticate" command
+      Authorization: `Bearer ${auth.access_token}`,
+      'Content-Type': 'application/json',
+    },
+  }).then((response) => response.json());
+
+  // 2. Find the current guild's info, including it's "icon"
+  const currentGuild = guilds.find((g) => g.id === discordSdk.guildId);
+
+  // 3. Append to the UI an img tag with the related information
+  if (currentGuild != null) {
+    const guildImg = document.createElement('img');
+    guildImg.setAttribute(
+      'src',
+      // More info on image formatting here: https://discord.com/developers/docs/reference#image-formatting
+      `https://cdn.discordapp.com/icons/${currentGuild.id}/${currentGuild.icon}.webp?size=128`
+    );
+    guildImg.setAttribute('width', '128px');
+    guildImg.setAttribute('height', '128px');
+    guildImg.setAttribute('style', 'border-radius: 50%;');
+    app.appendChild(guildImg);
+  }
 }
