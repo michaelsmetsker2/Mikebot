91669595595e2192ff09bd2cb5a36b6cda533d14
Michael Metsker <michaelsmetsker2@gmail.com>
2025-07-06 19:39:51 -0700

    fixed playfile script

--- playMessage.py
+++ playMessage.py
@@ -5,21 +5,25 @@ import time
 import subprocess
 
 FOLDER = "/mnt/pve/media/mikebot/temp"
-FILENAME = "annoy.wav"
+NAME = "annoy"
+EXTENSIONS = [".wav", ".mp3", ".m4a", ".aac", ".ogg", ".flac"]
 
-print(f"Waiting for {FILENAME} in {FOLDER}")
+print(f"Waiting for annoy file in {FOLDER}")
 
 while True:
-    path = os.path.join(FOLDER, FILENAME)
-    if os.path.isfile(path):
-        print(f"Found {path}, playing...")
-        subprocess.run(["ffplay", "-nodisp", "-autoexit", path]) #plays wav file with ffmpeg
+    
+    files = os.listdir(FOLDER)
+    for file in files:
+        if file.startswith(NAME) and any(file.endswith(ext) for ext in EXTENSIONS):
+            path = os.path.join(FOLDER, file)
+            print(f"Found {path}, playing...")
+            subprocess.run(["ffplay", "-nodisp", "-autoexit", path]) #plays wav file with ffmpeg
 
-        time.sleep(3) # TODO: make this wait long enough for the file lock to actually be gone
+            time.sleep(.5) # TODO: make this wait long enough for the file lock to actually be gone
 
-        try: #delete the file
-            os.remove(path)
-            print(f"Deleted {path}")
-        except Exception as e:
-            print(f"Failed to delete {path}: {e}")
+            try: #delete the file
+                os.remove(path)
+                print(f"Deleted {path}")
+            except Exception as e:
+                print(f"Failed to delete {path}: {e}")
     time.sleep(1.5)
