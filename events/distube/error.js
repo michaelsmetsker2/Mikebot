270c53f2219ac8279c27e25a57f19c54e152ca3c
Michael Metsker <michaelsmetsker2@gmail.com>
2025-09-11 12:56:35 -0700

    blurlple

--- events/distube/error.js
+++ events/distube/error.js
@@ -2,7 +2,7 @@ import { EmbedBuilder } from 'discord.js';
 
 export default async (error, queue) => {
     const embed = new EmbedBuilder()
-    .setColor(Blurple)
+    .setColor('Blurple')
     .setTitle("Error")
     .setDescription(`Sumthin bad happened: ${error}`);
     await queue.textChannel?.send({ embeds: [embed] });
