59bf8ac6e4e1a559b8387db5ec4bc24d552a566d
Michael Metsker <michaelsmetsker2@gmail.com>
2025-09-11 11:46:36 -0700

    unknown requester fix?>

--- events/distube/addSong.js
+++ events/distube/addSong.js
@@ -2,6 +2,8 @@
 import { EmbedBuilder } from 'discord.js';
 
 export default async (queue, song) => {
+    const requesterName = song.user?.globalName || song.user?.username || 'Unknown';
+    const requesterAvatar = song.user?.displayAvatarURL?.({ size: 1024 });
 
     const embed = new EmbedBuilder()
         .setAuthor({ name: 'MikeBot' })
@@ -9,8 +11,8 @@ export default async (queue, song) => {
         .setTitle('Song Added')
         .setDescription(`New song added to the queue\n**Song:** [${song.name} (${song.formattedDuration})](${song.url})`)
         .setFooter({
-            text: `Requested by ${song.user.globalName || song.user.username}`,
-            iconURL: song.user.displayAvatarURL({ size: 1024 }),
+            text: `Requested by ${requesterName}`,
+            iconURL: requesterAvatar,
         });
 
     await queue.textChannel?.send({ embeds: [embed] });
